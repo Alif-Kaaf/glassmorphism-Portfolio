0cb1c9be60d73c82c0088b8d7e9739e48c235c74
NewWeb <ahmedkhankk417@gmail.com>
2021-09-12 12:20:53 +0500

    PorfolioFirstCommit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/** @format */
+
+var type = new Typed(".typing-text", {
+  strings: ["web designer", "front end developer", "researcher"],
+  typeSpeed: 120,
+  loop: true,
+});
+let themeColor = document.querySelectorAll(".theme-toggler span");
+themeColor.forEach((color) =>
+  color.addEventListener("click", () => {
+    let background = color.style.background;
+    document.querySelector("body").style.background = background;
+  })
+);
